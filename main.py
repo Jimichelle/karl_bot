013d5ddd56dcad265a30fc1a388ebf18d85c3e93
Jimichelle <76010791+Jimichelle@users.noreply.github.com>
2024-03-16 20:22:42 +0100

    Update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+BEBOULAND_ID = os.getenv('BEBOULAND_ID')
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix="|",intents=intents)
@@ -28,22 +29,22 @@ async def on_ready():
     print("User ID : ", bot.user.id)
 
     try:
-        synced = await bot.tree.sync(guild=discord.Object(id=1138070170129149994))
+        synced = await bot.tree.sync(guild=discord.Object(id=BEBOULAND_ID))
         print(f"Synced {len(synced)} commands")
 
     except Exception as e:
         print(f"Failed to sync commands: {e}")
 
 
-@bot.tree.command(guild=discord.Object(id=1138070170129149994), name="test", description="Test command")
+@bot.tree.command(guild=discord.Object(id=BEBOULAND_ID), name="test", description="Test command")
 async def test_slash(interaction: discord.Interaction):
     await interaction.response.send_message("TEST!", ephemeral=True)
 
-@bot.tree.command(guild=discord.Object(id=1138070170129149994), name="bonjour", description="Test command")
+@bot.tree.command(guild=discord.Object(id=BEBOULAND_ID), name="bonjour", description="Test command")
 async def test_slash(interaction: discord.Interaction):
     await interaction.response.send_message("bonjour", ephemeral=True)
 
-@bot.tree.command(guild=discord.Object(id=1138070170129149994), name="random", description="Random")
+@bot.tree.command(guild=discord.Object(id=BEBOULAND_ID), name="random", description="Random")
 async def test_slash(interaction: discord.Interaction):
     rand = random.randint(1, 100)
     if rand < 50:
@@ -51,8 +52,13 @@ async def test_slash(interaction: discord.Interaction):
     else:
         await interaction.response.send_message("Gagné", ephemeral=True)
 
-@bot.command(name="sympatique", description="La commande trop sympatique")
-async def sympa_command(ctx):
-    await ctx.send("Va manger du caca")
-    
+@bot.tree.command(guild=discord.Object(id=BEBOULAND_ID), name="sympa", description="Je suis grave sympa")
+async def sympa_slash(interaction: discord.Interaction):
+    await interaction.response.send_message("Passe une agréable journée :)", ephemeral=True)
+
+@bot.tree.command(guild=discord.Object(id=BEBOULAND_ID), name="join", description="Join a voice channel")
+async def join_slash(interaction: discord.Interaction):
+    await interaction.response.send_message("Joining", ephemeral=True)
+    await interaction.guild.voice_channels[1].connect()
+
 bot.run(TOKEN)
